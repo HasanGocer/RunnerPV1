a07110461c5c8bafe2a4b33674970f29791f973f
Dennis Creevey <95920143+HasanGocer@users.noreply.github.com>
2023-02-02 00:10:32 +0300

    v 0.17

--- Assets/Scripts/FinishSystem.cs
+++ Assets/Scripts/FinishSystem.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class FinishSystem : MonoSingleton<FinishSystem>
 {
-    [SerializeField] private GameObject cameraPos;
+    [SerializeField] private GameObject cameraPos, cameraLookPos;
     [SerializeField] private int nowShakeCount, maxShakeCount;
     [SerializeField] private int finishRate;
 
-
+    public void FinishTime()
+    {
+        CamMoveControl.Instance.enabled = false;
+        Camera.main.gameObject.transform.position = cameraPos.transform.position;
+        Camera.main.gameObject.transform.LookAt(cameraLookPos.transform);
+    }
 }
